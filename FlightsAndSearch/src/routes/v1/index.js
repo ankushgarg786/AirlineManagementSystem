b761fbbcb3a7bd66eab83c92d1a3f18e8b153fde
Ankush Garg <ankushgarg7123@gmail.com>
2024-05-17 17:17:17 +0530

    Added Airplanes model

--- FlightsAndSearch/src/routes/v1/index.js
+++ FlightsAndSearch/src/routes/v1/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
-
-const cityController = require("../../conrollers/city-controller");
 const router = express.Router();
+
+// routes for city
+const cityController = require("../../conrollers/city-controller");
 router.post("/city", cityController.create);
 router.delete("/city/:id", cityController.destroy);
 router.patch("/city/:id", cityController.update);
 router.get("/city/:id", cityController.get);
 router.get("/city", cityController.getAll);
 
+//routes for airport
 const airportController = require("../../conrollers/airport-controller");
 router.post("/airport", airportController.create);
 router.get("/airport/:id", airportController.get);
 router.patch("/airport/:id", airportController.update);
 router.delete("/airport/:id", airportController.destroy);
+
 module.exports = router;
