414ed52200947aecebabc680e778394180f415f3
Ankush Garg <ankushgarg7123@gmail.com>
2024-06-02 16:46:50 +0530

    Added email and password validation for incoming request(middleware)

--- AuthService/SRC/controllers/user-controller.js
+++ AuthService/SRC/controllers/user-controller.js
@@ -24,6 +24,30 @@ const create = async (req, res) => {
     });
   }
 };
+
+const signIn = async (req, res) => {
+  try {
+    const response = await userService.signIn(
+      req.body.email,
+      req.body.password
+    );
+    return res.status(201).json({
+      data: response,
+      message: "successfully signed in",
+      success: true,
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "something went wrong",
+      data: {},
+      success: false,
+      err: error,
+    });
+  }
+};
 module.exports = {
   create,
+  signIn,
 };
--- AuthService/SRC/services/user-service.js
+++ AuthService/SRC/services/user-service.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 
 const UserRepository = require("../repository/user-repository");
 
-const { jwt_key } = require("../config/serverConfig");
+const { JWT_KEY } = require("../config/serverConfig");
 
 class UserService {
   constructor() {
@@ -18,9 +18,32 @@ class UserService {
       throw error;
     }
   }
+
+  async signIn(email, plainPassword) {
+    try {
+      //step 1 -> fetch the user using the email
+      const user = await this.userRepository.getByEmail(email);
+
+      //step 2 -> compare incoming plain password with stores encrypted password
+      const passwordsMatch = this.checkPassword(plainPassword, user.password);
+
+      if (!passwordsMatch) {
+        console.log("Password doesn't match");
+        throw { error: "Incorrect Password" };
+      }
+
+      //step 3 -> if passwords match then create a token and send it to the user
+      const newJWT = this.createToken({ email: user.email, id: user.id });
+      return newJWT;
+    } catch (error) {
+      console.log("Soething went wrong in the sign in process");
+      throw error;
+    }
+  }
+
   createToken(user) {
     try {
-      const result = jwt.sign(user, jwt_key, { expiresIn: "1d" });
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1d" });
       return result;
     } catch (error) {
       console.log("something went wrong in token creation");
@@ -30,7 +53,7 @@ class UserService {
 
   verifyToken(token) {
     try {
-      const response = jwt.verify(token, jwt_key);
+      const response = jwt.verify(token, JWT_KEY);
       return response;
     } catch (error) {
       console.log("something went wrong in token validation");
