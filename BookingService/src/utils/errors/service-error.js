1d501ec2466b5d36aee74a216397512ac66c51d6
Ankush Garg <ankushgarg7123@gmail.com>
2024-06-07 14:50:39 +0530

    Added Booking model

---
+++ BookingService/src/utils/errors/service-error.js
@@ -0,0 +1,16 @@
+const { StatusCodes, INTERNAL_SERVER_ERROR } = require("http-status-codes");
+
+class ServiceError extends Error {
+  constructor(
+    message = "something went Wrong",
+    explanation = "Service layer error",
+    statusCodes = StatusCodes.INTERNAL_SERVER_ERROR
+  ) {
+    this.name = "ServiceError";
+    this.message = message;
+    this.explanation = explanation;
+    this.statusCodes = statusCodes;
+  }
+}
+
+module.exports = ServiceError;
